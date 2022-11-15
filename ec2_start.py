29d57e3deb36fa3c8e4ca0fc2d33d17138d8a67a
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2022-11-15 18:20:14 +0530

    Update ec2_start.py

--- ec2_start.py
+++ ec2_start.py
@@ -1,6 +1,6 @@
 import boto3
 region = 'us-east-1'
-instances = ['i-001e2d04e37ccde3b']
+instances = ['i-0bee46cc060afd596','i-02bb1a43da72e25c7','i-01a0d3a6ebce41216']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
